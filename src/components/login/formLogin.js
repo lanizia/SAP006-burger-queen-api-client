cd32890926826b8d3c26eb4bd6a78f950677b8e2
tenorionique <monique_doretto@hotmail.com>
2021-09-13 11:10:06 -0300

    layout login

--- src/components/login/formLogin.js
+++ src/components/login/formLogin.js
@@ -1,6 +1,35 @@
-const formLogin = () => {
-    return <>
-    <h2>Login Page</h2>
-    </>;
-}
-export default formLogin
+import React, { useState }  from 'react';
+import loginLogo from "../../logocinza.gif";
+import './formLogin.css'
+
+const FormLogin = ({ Login, error }) => {
+    const [details, setDetails] = useState({email:"", password:""});
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        Login(details)
+    }
+
+    return (
+    <form onSubmit={submitHandler}>
+        <div className="form-inner">
+            <div className="image">
+                <img src={loginLogo} alt="logo"/>
+            </div>    
+            <h2>Login</h2>
+            {(error !=="") ? (<div className="error">{error}</div>) : ""}
+            <div className="form-group">
+                <label htmlFor="email">Email:</label>
+                <input type="email" placeholder="exemplo@exemplo.com" name="email" id="email" onChange={e=> setDetails ({...details, email: e.target.value})} value={details.email}/>
+            </div>  
+            <div className="form-group">
+                <label htmlFor="password">Password:</label>
+                <input type="password" placeholder="exemplo123" name="password" id="password" onChange={e=> setDetails ({...details, password: e.target.value})} value={details.password} />
+            </div>   
+            <input type="submit" value="LOGIN" />
+        </div>
+    </form>
+   )
+}   
+export default FormLogin
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,12 +1,56 @@
+import React, { useState } from 'react';
 import FormLogin from "../components/login/formLogin.js";
+import './index.css'
 
 
-const loginPage = () => {
+const LoginPage = async () => {
+  const adminUser = {
+    email: "admin@admin.com",
+    password: "admin123" 
+  }
+  /*let adminUser = {email, password};
+  let result = await fetch("https://lab-api-bq.herokuapp.com/auth", {
+    method: 'POST',
+    headers: {
+      "Content-Type":"application/json",
+      "Accept":"application/json"
+    },
+    body:JSON.stringify(adminUser),
+  });
+  result = await result.json(); */
+
+  const [user, setUser] = useState({email: ""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details)
+    if(details.email === adminUser.email && details.password === adminUser.password) {
+      console.log("Logged In");
+      setUser({
+        email: details.email
+      })
+    } else {
+      console.log("details do not match");
+      setError("details do not match");
+    }
+  }
+  const Logout = () => {
+    setUser ({email: ""});
+  }
     return (
-        <>
-         <h1>Hello</h1>
-         <FormLogin />
-        </>
+        <div className="AppLogin">
+          {(user.email !=="") ? (
+            <div className="welcome">
+              <h2> Welcome, <span>{user.email}</span></h2>
+              <button onClick={Logout}>Logout</button>
+            </div>
+          ) : (
+            <FormLogin Login={Login} error={error} />
+          )} 
+        </div>
       );
 }
-export default loginPage
+export default LoginPage;
+
+/*
+  */
