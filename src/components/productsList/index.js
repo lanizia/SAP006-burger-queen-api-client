b7abf30f773d25bb10f0b357577761d04c7b1fd9
Lana Souza <laniziamunique@gmail.com>
2021-09-22 09:56:02 -0300

    implementacao de lógica para nomear  complementos e sabores dos produtos

--- src/components/productsList/index.js
+++ src/components/productsList/index.js
@@ -1,37 +1,46 @@
-import {useEffect, useState} from 'react';
-import {formatMoney} from '../../services/formatMoney'; 
+import { useEffect, useState } from 'react';
+import { formatMoney } from '../../services/formatMoney';
 import { Button } from '../button/button';
-import { get } from '../../api/api'
+import { get } from '../../api/api';
 import './productsList.css';
 import MenuBtn from '../menuBtn/menuBtn';
 
 export const ProductList = ({ addToCart }) => {
-    const [products, setProducts] = useState([]);
-    const [menuType, setMenuType] = useState('all-day')
+  const [products, setProducts] = useState([]);
+  const [menuType, setMenuType] = useState('all-day');
 
-   const filterByMenuType = product => product.type === menuType;
+  const filterByMenuType = (product) => product.type === menuType;
 
-    useEffect(() => {
-        get('/products').then(setProducts);
-    }, [])
-     
-    return <>
-    <section className='btn-menu-content'>
-     <MenuBtn onChangeMenu={setMenuType}/>
-    </section>
-    <ul className='ProductsList'>
-        {products.filter(filterByMenuType).map(product => 
-            <li key={`product-${product.id}`}> 
-                <b>
-                    {product.name}
-                    {product.flavor ? ` tipo ${product.flavor}` : null}
-                    {product.complement ? ` com ${product.complement}` : null}
-                </b>
-                <img src={product.image} alt={product.name}></img>
-                <p>{formatMoney(product.price)}</p>
-                <Button type='button' value= 'adicionar' className='btn-adicionar' onClick={() => addToCart(product)}>Adicionar</Button>
-            </li>)}
-    </ul>
+  useEffect(() => {
+    get('/products').then(setProducts);
+  }, []);
+
+  return (
+    <>
+      <section className="btn-menu-content">
+        <MenuBtn onChangeMenu={setMenuType} />
+      </section>
+      <ul className="ProductsList">
+        {products.filter(filterByMenuType).map((product) => (
+          <li key={`product-${product.id}`}>
+            <b>
+              {product.name}
+              {product.flavor ? ` tipo ${product.flavor}` : null}
+              {product.complement ? ` com ${product.complement}` : null}
+            </b>
+            <img src={product.image} alt={product.name}></img>
+            <p>{formatMoney(product.price)}</p>
+            <Button
+              type="button"
+              value="adicionar"
+              className="btn-adicionar"
+              onClick={() => addToCart(product)}
+            >
+              Adicionar
+            </Button>
+          </li>
+        ))}
+      </ul>
     </>
-
-}
+  );
+};
